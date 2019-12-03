9634d1f1cd7128ce8cea318564c9ebbec0b077ff
Daniel Ma <drailskid@yahoo.com>
2019-12-02 22:27:49 -0800

    cleanup

--- day-1/solution.ts
+++ day-1/solution.ts
@@ -14,9 +14,6 @@ The Fuel Counter-Upper needs to know the total fuel requirement. To find it, ind
 What is the sum of the fuel requirements for all of the modules on your spacecraft?
 */
 
-import R from "ramda";
-const DEBUG_MODE = true;
-
 export function getFuel(mass: number): number {
   const result = Math.floor(mass / 3) - 2;
 
