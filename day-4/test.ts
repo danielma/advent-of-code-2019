ce0fb971655d511e99d6a0739811c279c64358be
Daniel Ma <drailskid@yahoo.com>
2019-12-06 22:56:01 -0800

    day 4 part 2

--- day-4/test.ts
+++ day-4/test.ts
@@ -1,5 +1,9 @@
 import test from "ava";
-import { isPasswordValid, stringToDigits } from "./solution";
+import {
+  isPasswordValid,
+  isPasswordValidPartTwo,
+  stringToDigits,
+} from "./solution";
 import R from "ramda";
 
 test("this is a valid password", t => {
@@ -30,6 +34,32 @@ test.skip("the real test", t => {
 
   const counts: { [key: string]: number } = R.countBy(
     R.pipe(isPasswordValid, (b: boolean) => (b ? "valid" : "invalid"))
+  )(passwords);
+
+  t.log(counts.valid);
+
+  t.is(1, 1);
+});
+
+test("part 2, 112233", t => {
+  t.is(isPasswordValidPartTwo("112233"), true);
+});
+
+test("part 2, 123444", t => {
+  t.is(isPasswordValidPartTwo("123444"), false);
+});
+
+test("part 2, 111122", t => {
+  t.is(isPasswordValidPartTwo("111122"), true);
+});
+
+test("part 2, the real test", t => {
+  const passwords = R.range(147981, 691424);
+
+  const counts: { [key: string]: number } = R.countBy(
+    R.pipe(R.toString, isPasswordValidPartTwo, (b: boolean) =>
+      b ? "valid" : "invalid"
+    )
   )(passwords);
 
   t.log(counts.valid);
