f45f47057c122ea16e6069a746726fd3ebc47221
Daniel Ma <drailskid@yahoo.com>
2019-12-07 13:59:01 -0800

    state can track inputs and outputs

--- shared/intcode.ts
+++ shared/intcode.ts
@@ -1,29 +1,40 @@
 import R from "ramda";
+import { parseIntBaseTen } from "../utils";
 
+type InstructionExecutor = (
+  inputs: number[],
+  args: { inputs: Inputs }
+) => { result?: number; outputs?: Outputs };
 export type Instruction = {
-  opCode: string;
+  opCode: number;
   arity: number;
-  execute: (inputs: number[]) => number;
+  execute: InstructionExecutor;
 };
 
 export type Program = number[];
 type Outputs = number[];
 type Inputs = number[];
-type ComputerResult = { program: Program; outputs: Outputs };
-type Computer = (program: Program, inputs?: Inputs) => ComputerResult;
+type Computer = (program: Program, inputs?: Inputs) => State;
 
 type State = {
   program: Program;
   cursor: number;
+  inputs: Inputs;
+  outputs: Outputs;
 };
 
-const getOpCode = R.pipe(R.toString, s => s.padStart(5, "0"), R.takeLast(2));
+const getOpCode = R.pipe(
+  R.toString,
+  s => s.padStart(5, "0"),
+  R.takeLast(2),
+  parseIntBaseTen
+);
 
 export function factory(...instructions: Instruction[]): Computer {
-  function step({ program, cursor }: State): State | null {
+  function step({ program, cursor, inputs, outputs }: State): State | null {
     const opCode = getOpCode(program[cursor]);
 
-    if (opCode === "99") return null;
+    if (opCode === 99) return null;
 
     const instruction = instructions.find(
       R.pipe(R.prop("opCode"), R.equals(opCode))
@@ -36,43 +47,59 @@ export function factory(...instructions: Instruction[]): Computer {
       );
       const location = program[cursor + 1 + instruction.arity];
 
-      program[location] = instruction.execute(args);
+      const instructionResult = instruction.execute(args, {
+        inputs: inputs,
+      });
+
+      if (instructionResult.result !== undefined) {
+        program[location] = instructionResult.result;
+      }
+
+      if (instructionResult.outputs !== undefined) {
+        outputs.push(...instructionResult.outputs);
+      }
 
       return {
         cursor: cursor + instruction.arity + 2,
         program,
+        inputs,
+        outputs,
       };
     } else {
       return null;
     }
   }
 
-  function _intCode(state: State): Program {
+  function _intCode(state: State): State {
     const result = step(state);
 
     if (result) {
       return _intCode(result);
     } else {
-      return state.program;
+      return state;
     }
   }
 
-  return function intCode(
-    program: Program,
-    inputs: Inputs = []
-  ): ComputerResult {
-    return { program: _intCode({ program, cursor: 0 }), outputs: [] };
+  return function intCode(program: Program, inputs: Inputs = []): State {
+    return _intCode({ program, cursor: 0, inputs, outputs: [] });
   };
 }
 
+const computeResult: (
+  computer: (inputs: number[]) => number
+) => InstructionExecutor = computer => {
+  return inputs => {
+    return { result: computer(inputs) };
+  };
+};
 export const Add: Instruction = {
-  opCode: "01",
+  opCode: 1,
   arity: 2,
-  execute: R.sum,
+  execute: computeResult(R.sum),
 };
 
 export const Multiply: Instruction = {
-  opCode: "02",
+  opCode: 2,
   arity: 2,
-  execute: R.reduce((acc, n) => acc * n, 1),
+  execute: computeResult(R.reduce((acc, n) => acc * n, 1)),
 };
