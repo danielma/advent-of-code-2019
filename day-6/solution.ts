88b3a5ba2024f155a7320346d6458537c3b8d301
Daniel Ma <drailskid@yahoo.com>
2019-12-07 16:13:14 -0800

    day 6 was incredibly naive

--- day-6/solution.ts
+++ day-6/solution.ts
@@ -1,12 +1,13 @@
 import R from "ramda";
-// type OrbitCounts = { direct: number; indirect: number };
 
 type Planet = {
   name: string;
   orbits: string;
 };
 
-export function countOrbits(map: string): number {
+type ReverseMap = { [key: string]: Planet["name"] };
+
+function makeReverseMap(map: string): ReverseMap {
   const planets: Planet[] = map.split("\n").map(o => {
     return {
       name: o.split(")")[1],
@@ -14,7 +15,7 @@ export function countOrbits(map: string): number {
     };
   });
 
-  const reverseMap = R.reduce(
+  return R.reduce(
     (acc: object, planet: Planet) => {
       acc[planet.name] = planet.orbits;
       return acc;
@@ -22,17 +23,74 @@ export function countOrbits(map: string): number {
     {},
     planets
   );
+}
+
+export function countOrbits(map: string): number {
+  const reverseMap = makeReverseMap(map);
 
   let count = 0;
 
-  R.mapObjIndexed(orbits => {
+  R.forEach(orbits => {
     let nextPlanet = orbits;
     do {
       count++;
       nextPlanet = reverseMap[nextPlanet];
     } while (nextPlanet);
-  }, reverseMap);
+  }, R.values(reverseMap));
 
   return count;
-  // return { direct: 0, indirect: 0 };
+}
+
+function makePath({
+  from,
+  to = undefined,
+  map,
+}: {
+  from: Planet["name"];
+  to?: Planet["name"];
+  map: ReverseMap;
+}): Planet["name"][] {
+  const path = [];
+
+  let nextPlanet = map[from];
+
+  do {
+    path.push(nextPlanet);
+    nextPlanet = map[nextPlanet];
+  } while (nextPlanet && nextPlanet !== to);
+
+  return path;
+}
+
+function findTheFirstCommonElement(pathA: string[], pathB: string[]): string {
+  for (let iA = 0; iA < pathA.length; iA++) {
+    const planetA = pathA[iA];
+    for (let iB = 0; iB < pathB.length; iB++) {
+      const planetB = pathB[iB];
+
+      if (planetA === planetB) {
+        return planetA;
+      }
+    }
+  }
+
+  return "COM";
+}
+
+export function transfersFrom(
+  planetA: string,
+  planetB: string,
+  map: string
+): number {
+  const reverseMap = makeReverseMap(map);
+
+  const pathA = makePath({ from: planetA, map: reverseMap });
+  const pathB = makePath({ from: planetB, map: reverseMap });
+
+  const common = findTheFirstCommonElement(pathA, pathB);
+
+  return (
+    makePath({ from: planetA, to: common, map: reverseMap }).length +
+    makePath({ from: planetB, to: common, map: reverseMap }).length
+  );
 }
